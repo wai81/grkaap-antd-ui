51391d296f0bdc11d11b71543119c3a2fe65ae2b
wai81 <50743966+wai81@users.noreply.github.com>
2023-02-25 13:31:46 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/reset.css";
+
+import dataProvider from "@pankod/refine-simple-rest";
+import { AntdInferencer } from "@pankod/refine-inferencer/antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { useTranslation } from "react-i18next";
+import { RefineKbarProvider } from "@pankod/refine-kbar";
+import { ColorModeContextProvider } from "contexts";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <ColorModeContextProvider>
+      <RefineKbarProvider>
+        <Refine
+          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          notificationProvider={notificationProvider}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          resources={[
+            {
+              name: "posts",
+              list: AntdInferencer,
+              edit: AntdInferencer,
+              show: AntdInferencer,
+              create: AntdInferencer,
+              canDelete: true,
+            },
+          ]}
+          Title={Title}
+          Header={Header}
+          Sider={Sider}
+          Footer={Footer}
+          Layout={Layout}
+          OffLayoutArea={OffLayoutArea}
+          routerProvider={routerProvider}
+          i18nProvider={i18nProvider}
+        />
+      </RefineKbarProvider>
+    </ColorModeContextProvider>
+  );
+}
+
+export default App;
